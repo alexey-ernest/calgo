33e134cfab3c4fc881528bd63463b337bc445ce5
alexey-ernest <alexey.ernest@gmail.com>
2023-07-01 15:34:53 -0700

    small improvements

--- sort/gen_random.c
+++ sort/gen_random.c
@@ -1,4 +1,4 @@
-/* Generate a pseudo-random uint array wtih values between 0 and RAND_MAX 
+/* Generate a pseudo-random uint array with values between 0 and RAND_MAX 
 and print it to stdout */
 
 #include <time.h>
