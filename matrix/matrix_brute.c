104786b569cac5e5ab931815292cefe0f1ad997c
alexey-ernest <alexey.ernest@gmail.com>
2023-07-15 10:45:00 -0700

    matrix multiplication, brute force solution in O(n^3)

---
+++ matrix/matrix_brute.c
@@ -0,0 +1,83 @@
+/* Matrix multiplication, brute force (O(n^3)) */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_VAL	1000
+
+struct _matrix {
+	int rows;
+	int cols;
+	int **data;
+};
+
+struct _matrix* _gen_rand_matrix(int);
+struct _matrix* _mul_square(struct _matrix*, struct _matrix*);
+
+int main(int argc, char const *argv[])
+{
+	if (argc < 2) {
+		printf("usage: matrix_brute 100\n");
+		return 1;
+	}
+
+	int n = atoi(argv[1]);
+
+	struct _matrix *m1 = _gen_rand_matrix(n);
+	struct _matrix *m2 = _gen_rand_matrix(n);
+	struct _matrix *mul = _mul_square(m1, m2);
+	// int i, j;
+	// for (i = 0; i < n; i++) {
+	// 	for (j = 0; j < n; j++) {
+	// 		printf("%10i ", mul->data[i][j]);
+	// 	}
+	// 	printf("\n");
+	// }
+	return 0;
+}
+
+struct _matrix* _mul_square(struct _matrix *m1, struct _matrix *m2) {
+	if (m1->rows != m1->cols || m2->rows != m2->cols || m1->rows != m2->rows) {
+		return NULL;
+	}
+	int n = m1->rows;
+
+	struct _matrix *res = (struct _matrix*)malloc(sizeof(struct _matrix));
+	res->rows = m1->rows;
+	res->cols = m2->cols;
+	res->data = (int**)malloc(n * sizeof(int*));
+
+	int i, j, k;
+	for (i = 0; i < n; i++) {
+		res->data[i] = (int*)malloc(n * sizeof(int));
+		for (j = 0; j < n; j++) {
+			res->data[i][j] = 0;
+			for (k = 0; k < n; k++) {
+				res->data[i][j] += m1->data[i][k] * m2->data[k][j];
+			}
+		}
+	}
+
+	return res;
+}
+
+struct _matrix* _gen_rand_matrix(int n) {
+	// init random generator
+	srand(time(NULL));
+	int max_int = (int)(~(0u) >> 1);
+
+	int i, j;
+	struct _matrix *res = (struct _matrix*)malloc(sizeof(struct _matrix));
+	res->rows = n;
+	res->cols = n;
+	res->data = (int**)malloc(n * sizeof(int*));
+	for (i = 0; i < n; i++) {
+		res->data[i] = (int*)malloc(n * sizeof(int));
+		for (j = 0; j < n; j++) {
+			res->data[i][j] = (float)rand() / (float)max_int * MAX_VAL;
+		}
+	}
+
+	return res;
+}
